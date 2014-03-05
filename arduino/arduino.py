3523c52e3a949c6762b518e5a28f6c84fa145ad2
Christian Aurich (I2SE GmbH) <christian.aurich@i2se.com>
2014-03-05 09:27:59 +0100

    updated arduino library to be compatible to latest changes

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -8,12 +8,13 @@ class Arduino(object):
 
     def __init__(self, port, baudrate=115200):
         self.serial = serial.Serial(port, baudrate)
-        self.serial.write('99')
+        self.serial.write('100')
 
     def __str__(self):
         return "Arduino is on port %s at %d baudrate" %(self.serial.port, self.serial.baudrate)
 
     def output(self, pinArray):
+        self.__sendData('99')
         self.__sendData(len(pinArray))
 
         if(isinstance(pinArray, list) or isinstance(pinArray, tuple)):
@@ -46,20 +47,31 @@ class Arduino(object):
     def analogRead(self, pin):
         self.__sendData('4')
         self.__sendData(pin)
-        return self.__getData()
+        return int(self.__getData())
 
     def turnOff(self):
         for each_pin in self.__OUTPUT_PINS:
             self.setLow(each_pin)
         return True
 
+    def getID(self):
+        self.__sendData('97')
+        return self.__getData()
+
+    def setID(self, ID):
+        self.__sendData('98')
+        self.__sendData(ID)
+        return True
+
     def __sendData(self, serial_data):
         while(self.__getData()[0] != "w"):
             pass
+        #print("writing data: " + str(serial_data))
         self.serial.write(str(serial_data))
 
     def __getData(self):
-        return self.serial.readline().rstrip('\n')
+        #print("reading data")
+        return self.serial.readline().rstrip('\n').rstrip('\r')
 
     def __formatPinState(self, pinValue):
         if pinValue == '1':
